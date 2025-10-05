43ccb679f350fd6761d45ca7f3830ce3749f58ba
Masahiro KIYOOKA <kiyookamasa@gmail.com>
2025-10-05 16:52:11 +0900

    chore: clang-tidyチェックからdoctestを除外

--- cmake/dependencies-test.cmake
+++ cmake/dependencies-test.cmake
@@ -8,3 +8,13 @@ declare_fetchcontent_with_local(doctest ext/doctest-2.4.11
     GIT_TAG v2.4.11
 )
 FetchContent_MakeAvailable(doctest)
+
+# Mark doctest as system library to exclude it from clang-tidy checks
+if(TARGET doctest)
+    get_target_property(doctest_include_dirs doctest INTERFACE_INCLUDE_DIRECTORIES)
+    if(doctest_include_dirs)
+        set_target_properties(doctest PROPERTIES
+            INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${doctest_include_dirs}"
+        )
+    endif()
+endif()
--- cmake/quality-setup.cmake
+++ cmake/quality-setup.cmake
@@ -24,10 +24,12 @@ file(GLOB_RECURSE COMPILABLE_SOURCE_FILES
     ${CMAKE_SOURCE_DIR}/tests/**/*.cpp
 )
 
-# Remove ext/ directory files from quality tools processing
-file(GLOB_RECURSE EXT_FILES ${CMAKE_SOURCE_DIR}/ext/**)
-list(REMOVE_ITEM ALL_SOURCE_FILES ${EXT_FILES})
-list(REMOVE_ITEM COMPILABLE_SOURCE_FILES ${EXT_FILES})
+# Remove third-party directory files from quality tools processing
+file(GLOB_RECURSE THIRD_PARTY_FILES ${CMAKE_SOURCE_DIR}/third_party/**)
+file(GLOB_RECURSE EXTERNAL_FILES ${CMAKE_SOURCE_DIR}/external/**)
+
+list(REMOVE_ITEM ALL_SOURCE_FILES ${THIRD_PARTY_FILES} ${EXTERNAL_FILES})
+list(REMOVE_ITEM COMPILABLE_SOURCE_FILES ${THIRD_PARTY_FILES} ${EXTERNAL_FILES})
 
 # Setup quality management tools
 include(cmake/quality-tools.cmake)
